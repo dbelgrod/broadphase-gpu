325781efea62b3fb3da4e1d26ff5ef18403bdbd6
dbelgrod <davidbelgrod@gmail.com>
2021-11-17 09:46:44 -0500

    changing header file ext name

--- src/simulation.cuh
+++ src/simulation.cuh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <gpubf/sweep.cuh>
+#include <gpubf/timer.cuh>
 // #include <gpubf/util.cuh>
 
 void run_collision_counter(Aabb* boxes, int N);
@@ -9,3 +10,5 @@ void run_collision_counter(Aabb* boxes, int N);
 void run_sweep_multigpu(const Aabb* boxes, int N, int nbox, vector<pair<int,int>>& finOverlaps, int& threads, int& devcount);
 
 void run_sweep_pieces(const Aabb* boxes, int N, int nbox, vector<pair<int, int>>& finOverlaps, int& threads, int & devcount);
+
+void run_sweep_pairing(const Aabb* boxes, int N, int nbox, vector<pair<int, int>>& finOverlaps, int& threads, int & devcount);
