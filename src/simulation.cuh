f59f3b9caa9a6a6a8bfdd547c0b89c0011b1239a
dbelgrod <davidbelgrod@gmail.com>
2021-11-22 16:27:08 -0500

    hiding weird bug for now

--- src/simulation.cuh
+++ src/simulation.cuh
@@ -2,7 +2,7 @@
 
 #include <gpubf/sweep.cuh>
 #include <gpubf/timer.cuh>
-#include <gpubf/util.cuh>
+// #include <gpubf/util.cuh>
 
 void run_collision_counter(Aabb* boxes, int N);
 // void run_scaling(const Aabb* boxes, int N, int nBox, vector<unsigned long>& overlaps);
