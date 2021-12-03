d71544ee6f8ac5be86badcc8afe51f4ca3d47b2f
dbelgrod <davidbelgrod@gmail.com>
2021-12-03 11:30:09 -0500

    moving around header files

--- src/io.cuh
+++ src/io.cuh
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <gpubf/aabb.cuh>
 
 void constructBoxes
@@ -7,12 +9,7 @@ void constructBoxes
     Eigen::MatrixXi& faces, 
     Eigen::MatrixXi& edges, 
     vector<Aabb>& boxes
-)
-{
-    addVertices(vertices_t0, vertices_t1, boxes);
-    addEdges(vertices_t0, vertices_t1, edges, boxes);
-    addFaces(vertices_t0, vertices_t1, faces, boxes);
-}
+);
 
 void parseMesh(
     const char* filet0, 
@@ -20,29 +17,4 @@ void parseMesh(
     Eigen::MatrixXd& V0, 
     Eigen::MatrixXd& V1, 
     Eigen::MatrixXi& F, 
-    Eigen::MatrixXi& E)
-{
-    // Eigen::MatrixXd V0;
-    // Eigen::MatrixXd V1;
-    // Eigen::MatrixXi F;
-
-    string fn = string(filet0);
-    string ext = fn.substr(fn.rfind('.') + 1);
-
-    if(ext == "obj") 
-    {
-        igl::readOBJ(filet0, V0, F);
-        igl::readOBJ(filet1, V1, F);
-    }
-    else 
-    {
-        igl::readPLY(filet0, V0, F);
-        igl::readPLY(filet1, V1, F);
-    }
-
-    
-    // Eigen::MatrixXi E;
-    igl::edges(F,E);
-    // faces should be same F^{t=0} = F^{t=1}
-    // constructBoxes(V0, V1, F, E, boxes);
-}
+    Eigen::MatrixXi& E);
