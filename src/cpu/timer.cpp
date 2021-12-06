1f3cbd278589ccedf18b2a3852214263b3e63e72
dbelgrod <davidbelgrod@gmail.com>
2021-12-06 18:12:29 -0500

    moving recorder to cpp file

---
+++ src/cpu/timer.cpp
@@ -0,0 +1,49 @@
+#include <gpubf/timer.hpp>
+#include <iostream>
+using json = nlohmann::json;
+
+namespace ccd {
+
+struct Record
+{
+      Timer timer;
+      char * tag;
+      json j_object;
+
+      Record(){};
+
+      void Start(char * s)
+      {
+            tag = s;
+            timer.start();
+      }
+
+      void Start(char * s, json & jtmp)
+      {
+           j_object = jtmp;
+           Start(s);
+      } 
+
+      void Stop()
+      {
+            timer.stop();
+            double elapsed = 0;
+            elapsed += timer.getElapsedTimeInMicroSec();
+            // j_object[tag]=elapsed;
+            j_object.push_back(json::object_t::value_type(tag, elapsed));
+            printf("%s : %.3f ms\n", tag, elapsed / 1000.f);
+      }
+
+      void Print()
+      {
+            cout << j_object.dump() << endl;  
+      }
+      
+      json Dump()
+      {
+            return j_object;
+      }
+};
+
+
+}
--- src/cpu/timer.hpp
+++ src/cpu/timer.hpp
@@ -15,10 +15,6 @@
 
 #pragma once
 
-#include <iostream>
-// #include <nlohmann/json.hpp>
-// using json = nlohmann::json;
-
 #ifdef WIN32   // Windows system specific
 #include <windows.h>
 #elif __APPLE__ // Unix based system specific
@@ -190,47 +186,5 @@ void recordLaunch(char* tag, void(*f)(Arguments...), Arguments... args) {
       elapsed += timer.getElapsedTimeInMicroSec();
       printf("%s : %.6f ms\n", tag, elapsed );
 };
-
-struct Record
-{
-      Timer timer;
-      char * tag;
-      json j_object;
-
-      Record(){};
-
-      void Start(char * s)
-      {
-            tag = s;
-            timer.start();
-      }
-
-      void Start(char * s, json & jtmp)
-      {
-           j_object = jtmp;
-           Start(s);
-      } 
-
-      void Stop()
-      {
-            timer.stop();
-            double elapsed = 0;
-            elapsed += timer.getElapsedTimeInMicroSec();
-            // j_object[tag]=elapsed;
-            j_object.push_back(json::object_t::value_type(tag, elapsed));
-            printf("%s : %.3f ms\n", tag, elapsed / 1000.f);
-      }
-
-      void Print()
-      {
-            cout << j_object.dump() << endl;  
-      }
-      
-      json Dump()
-      {
-            return j_object;
-      }
-};
-
 
 }
