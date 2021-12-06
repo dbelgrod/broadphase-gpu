f72ff102408230327c146725d0c54bf5a26fff6d
dbelgrod <davidbelgrod@gmail.com>
2021-12-06 05:09:37 -0500

    adding primative checks

--- src/cpu/sweep.hpp
+++ src/cpu/sweep.hpp
@@ -7,6 +7,14 @@ using namespace std;
 
 namespace ccdcpu {
 
+bool is_face(const int* vids);
+
+bool is_edge(const int* vids);
+
+bool is_vertex(const int* vids);
+
+bool is_valid_pair(const int* a, const int* b);
+
 void run_sweep_cpu(
     vector<Aabb>& boxes, 
     int N, int numBoxes, 
