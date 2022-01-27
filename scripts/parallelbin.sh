5b8e488b9ac42bb515ba64a91d9a8d7ea19bd5a7
dbelgrod <davidbelgrod@gmail.com>
2022-01-27 13:07:33 -0500

    rm scripts

--- scripts/parallelbin.sh
+++
@@ -1,6 +0,0 @@
-END=$2
-PRL=$1
-# echo "Running exe $2 in parallel with $1 threads each"
-for ((i=1;i<=END;i++)); do
-    /home/dbelgrod/bruteforce-gpu/build/cpusweep_bin /home/dbelgrod/dataset/UNC-Dynamic-Scene-Benchmarks/cloth-ball/cloth_ball92.ply /home/dbelgrod/dataset/UNC-Dynamic-Scene-Benchmarks/cloth-ball/cloth_ball93.ply -p $((PRL)) &
-done 
